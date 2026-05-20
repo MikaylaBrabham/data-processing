fff977cffe5cce672b4503ca0d018a2aa4af49a5
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-20 15:30:44 -0500

    added people and psuedo code for next step of prompt users for name

--- src/main/java/com/pluralsight/loops/Program.java
+++ src/main/java/com/pluralsight/loops/Program.java
@@ -7,15 +7,19 @@ public class Program {
 
         // create a list of 10 people only using traditional loops
         Person person1 = new Person("Huey", "Freeman", 10);
-        Person person2 = new Person("Huey", "Freeman", 10);
-        Person person3 = new Person("Huey", "Freeman", 10);
-        Person person1 = new Person("Huey", "Freeman", 10);
-        Person person1 = new Person("Huey", "Freeman", 10);
-        Person person1 = new Person("Huey", "Freeman", 10);
-        Person person1 = new Person("Huey", "Freeman", 10);
-        Person person1 = new Person("Huey", "Freeman", 10);
-        Person person1 = new Person("Huey", "Freeman", 10);
-        Person person1 = new Person("Huey", "Freeman", 10);
+        Person person2 = new Person("Riley", "Freeman", 8);
+        Person person3 = new Person("Robert", "Freeman", 62);
+        Person person4 = new Person("Sarah", "Dubois", 35);
+        Person person5 = new Person("Uncle", "Ruckus", 5);
+        Person person6 = new Person("Cindy", "McPhearson", 9);
+        Person person7 = new Person("A Pimp ", "Named Slickback", 35);
+        Person person8 = new Person("Thug", "Nificent", 37);
+        Person person9 = new Person("Gangsta", "Licious", 35);
+        Person person10 = new Person("Joe", "Petto", 71);
+
+
+        // insert scanner and prompt user for a name to search
+
 
 
     }
