91e459eda94524ccc2d0ed4632b4802d2029596f
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-20 15:21:06 -0500

    craeted first person and copied it to change for next 9 ppl

--- src/main/java/com/pluralsight/loops/Program.java
+++ src/main/java/com/pluralsight/loops/Program.java
@@ -1,8 +1,22 @@
 package com.pluralsight.loops;
 
+import java.util.List;
+
 public class Program {
     public static void main(String[] args) {
 
-        // create a list of 10 people
+        // create a list of 10 people only using traditional loops
+        Person person1 = new Person("Huey", "Freeman", 10);
+        Person person2 = new Person("Huey", "Freeman", 10);
+        Person person3 = new Person("Huey", "Freeman", 10);
+        Person person1 = new Person("Huey", "Freeman", 10);
+        Person person1 = new Person("Huey", "Freeman", 10);
+        Person person1 = new Person("Huey", "Freeman", 10);
+        Person person1 = new Person("Huey", "Freeman", 10);
+        Person person1 = new Person("Huey", "Freeman", 10);
+        Person person1 = new Person("Huey", "Freeman", 10);
+        Person person1 = new Person("Huey", "Freeman", 10);
+
+
     }
 }
