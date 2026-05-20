0bd6be3fbe20c2e3b3e4e6d79ad29015e56da124
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-20 15:11:00 -0500

    added all varaibles given with constructors and getters and setters

--- src/main/java/com/pluralsight/loops/Person.java
+++ src/main/java/com/pluralsight/loops/Person.java
@@ -1,6 +1,44 @@
 package com.pluralsight.loops;
 
 public class Person {
-    
-    private string firstName();
+
+        //add given string
+        private String FirstName;
+        private String LastName;
+        private Integer age;
+
+
+        // add constructor
+    public Person(String firstName, String lastName, Integer age) {
+        FirstName = firstName;
+        LastName = lastName;
+        this.age = age;
+    }
+
+    //add getters and setters
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
 }
